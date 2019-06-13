24e5885bda978b6e230438f510bf085d64d3149e
Michael Mackus <michaelmackus@gmail.com>
2019-06-12 18:56:43 -0700

    Make map reading safer
    Map bounds checking & doesn't assume newline.

--- game.c
+++ game.c
@@ -35,25 +35,48 @@ Sprite* get_enemies()
 
 int init_game()
 {
+
     player.pos.x = 9.5;
     player.pos.y = 9.5;
     player.dir = to_radians(90);
     player.fov = to_radians(90);
 
     // read map from file
-    FILE *f = fopen("map.txt", "r");
-    if (f == NULL) return 1;
-    char *tmp = map; // current map buffer
-    for (int y = 0; y < MAP_HEIGHT; ++y)
     {
-        // read MAP_WIDTH characters into map buffer
-        if (fread(tmp, sizeof(*tmp), MAP_WIDTH, f) == 0) {
+        FILE *f = fopen("map.txt", "r");
+        if (f == NULL)
+        {
             return 1;
         }
-        // skip newline
-        fseek(f, 1, SEEK_CUR);
-        // advance map buffer MAP_WIDTH characters
-        tmp += MAP_WIDTH;
+
+        // curMap is our current map buffer (to write), line is the current file read buffer
+        char *curMap = map;
+        char line[MAP_WIDTH];
+
+        for (int bytesRead = 0; bytesRead < MAP_WIDTH*MAP_HEIGHT;)
+        {
+            // read bytesToRead characters into map buffer
+            int bytesToRead = MAP_WIDTH;
+            if (bytesRead + bytesToRead > MAP_WIDTH*MAP_HEIGHT)
+            {
+                bytesToRead = MAP_WIDTH*MAP_HEIGHT - bytesRead;
+            }
+            int size = fread(line, sizeof(*line), bytesToRead, f);
+
+            // EOF before end of map
+            if (size == 0)
+                return 1;
+
+            for (int i = 0; i < size; ++i)
+            {
+                // don't append to map if newline
+                if (line[i] == '\n') continue;
+
+                *curMap = line[i];
+                curMap ++;
+                bytesRead ++;
+            }
+        }
     }
 
     return 0;
